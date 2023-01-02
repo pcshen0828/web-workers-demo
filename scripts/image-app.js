35817e0f65c2495f383d59a55a64e6efbe344d37
pcshen0828 <michelleshen0828@gmail.com>
2023-01-03 00:09:58 +0800

    Handle image with web worker.

--- scripts/image-app.js
+++ scripts/image-app.js
@@ -1,37 +1,39 @@
-(function(){
+(function () {
   // http://stackoverflow.com/questions/10906734/how-to-upload-image-into-html5-canvas
   var original;
-  var imageLoader = document.querySelector('#imageLoader');
-  imageLoader.addEventListener('change', handleImage, false);
-  var canvas = document.querySelector('#image');
-  var ctx = canvas.getContext('2d');
+  var imageLoader = document.querySelector("#imageLoader");
+  imageLoader.addEventListener("change", handleImage, false);
+  var canvas = document.querySelector("#image");
+  var ctx = canvas.getContext("2d");
 
-  function handleImage(e){
+  var imageWorker = new Worker("scripts/worker.js");
+
+  function handleImage(e) {
     var reader = new FileReader();
-    reader.onload = function(event){
+    reader.onload = function (event) {
       var img = new Image();
-      img.onload = function(){
+      img.onload = function () {
         canvas.width = img.width;
         canvas.height = img.height;
-        ctx.drawImage(img,0,0);
+        ctx.drawImage(img, 0, 0);
         original = ctx.getImageData(0, 0, canvas.width, canvas.height);
-      }
+      };
       img.src = event.target.result;
-    }
+    };
     reader.readAsDataURL(e.target.files[0]);
   }
 
   // greys out the buttons while manipulation is happening
   // un-greys out the buttons when the manipulation is done
   function toggleButtonsAbledness() {
-    var buttons = document.querySelectorAll('button');
+    var buttons = document.querySelectorAll("button");
     for (var i = 0; i < buttons.length; i++) {
-      if (buttons[i].hasAttribute('disabled')) {
-        buttons[i].removeAttribute('disabled')
+      if (buttons[i].hasAttribute("disabled")) {
+        buttons[i].removeAttribute("disabled");
       } else {
-        buttons[i].setAttribute('disabled', null);
+        buttons[i].setAttribute("disabled", null);
       }
-    };
+    }
   }
 
   function manipulateImage(type) {
@@ -43,39 +45,43 @@
     // Hint! This is where you should post messages to the web worker and
     // receive messages from the web worker.
 
-    length = imageData.data.length / 4;
-    for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
-      r = imageData.data[i * 4 + 0];
-      g = imageData.data[i * 4 + 1];
-      b = imageData.data[i * 4 + 2];
-      a = imageData.data[i * 4 + 3];
-      pixel = manipulate(type, r, g, b, a);
-      imageData.data[i * 4 + 0] = pixel[0];
-      imageData.data[i * 4 + 1] = pixel[1];
-      imageData.data[i * 4 + 2] = pixel[2];
-      imageData.data[i * 4 + 3] = pixel[3];
-    }
-    toggleButtonsAbledness();
-    return ctx.putImageData(imageData, 0, 0);
-  };
+    imageWorker.postMessage({ imageData, type });
+
+    imageWorker.onmessage = function (e) {
+      toggleButtonsAbledness();
+      var image = e.data;
+      if (image) return ctx.putImageData(image, 0, 0);
+      console.log("No manipulated image returned.");
+    };
+
+    imageWorker.onerror = function (error) {
+      class WorkerException {
+        constructor(message) {
+          this.name = "WorkerException";
+          this.message = message;
+          throw new WorkerException("Worker error.");
+        }
+      }
+    };
+  }
 
   function revertImage() {
     return ctx.putImageData(original, 0, 0);
   }
 
-  document.querySelector('#invert').onclick = function() {
+  document.querySelector("#invert").onclick = function () {
     manipulateImage("invert");
   };
-  document.querySelector('#chroma').onclick = function() {
+  document.querySelector("#chroma").onclick = function () {
     manipulateImage("chroma");
   };
-  document.querySelector('#greyscale').onclick = function() {
+  document.querySelector("#greyscale").onclick = function () {
     manipulateImage("greyscale");
   };
-  document.querySelector('#vibrant').onclick = function() {
+  document.querySelector("#vibrant").onclick = function () {
     manipulateImage("vibrant");
   };
-  document.querySelector('#revert').onclick = function() {
+  document.querySelector("#revert").onclick = function () {
     revertImage();
   };
 })();
