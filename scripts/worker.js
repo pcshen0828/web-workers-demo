0ec9a8cbb409134f732926b6b9a1701358a225ee
pcshen0828 <michelleshen0828@gmail.com>
2023-01-03 00:22:57 +0800

    Move out function declaration from the for loop.

--- scripts/worker.js
+++ scripts/worker.js
@@ -1,17 +1,22 @@
-importScripts('imageManips.js');
+importScripts("imageManips.js");
 
-this.onmessage = function(e) {
+this.onmessage = function (e) {
   var imageData = e.data.imageData;
   var type = e.data.type;
 
   try {
     length = imageData.data.length / 4;
-    for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
+    var manipulatePixel = getManipulateFunc(type);
+    for (
+      i = j = 0, ref = length;
+      0 <= ref ? j <= ref : j >= ref;
+      i = 0 <= ref ? ++j : --j
+    ) {
       r = imageData.data[i * 4 + 0];
       g = imageData.data[i * 4 + 1];
       b = imageData.data[i * 4 + 2];
       a = imageData.data[i * 4 + 3];
-      pixel = manipulate(type, r, g, b, a);
+      pixel = manipulatePixel(r, g, b, a);
       imageData.data[i * 4 + 0] = pixel[0];
       imageData.data[i * 4 + 1] = pixel[1];
       imageData.data[i * 4 + 2] = pixel[2];
@@ -22,8 +27,8 @@ this.onmessage = function(e) {
     function ManipulationException(message) {
       this.name = "ManipulationException";
       this.message = message;
-    };
-    throw new ManipulationException('Image manipulation error');
+    }
+    throw new ManipulationException("Image manipulation error");
     postMessage(undefined);
   }
-}
+};
